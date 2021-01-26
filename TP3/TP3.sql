a5a472510a2c1257224b49f0a224c68dfbc45825
victorsed <48103974+victorsed@users.noreply.github.com>
2021-01-26 15:59:47 +0100

    Add files via upload

--- TP3/TP3.sql
+++ TP3/TP3.sql
@@ -5,55 +5,67 @@
 # S : imported 25357 documents 
 
 # R :
-use restaurant 
-db.createCollection("restaurants")
+> use restaurant 
+> db.createCollection("restaurants")
 
-mongoimport -d db_agg -c restaurants --drop --file "D:/Cours/B3/NoSQL/TP3/tp3json/restaurants.json"
+> mongoimport -d db_agg -c restaurants --drop --file "D:/Cours/B3/NoSQL/TP3/tp3json/restaurants.json"
 
 Répondre aux questions suivantes : 
 # Q : La répartition des restaurants par quartier ? 
 # Indice : quartier => key 'borough' 
 
-db.restaurants.aggregate( [
-    { $project : {
-        "name":1, "borough":1, "_id":0
-    }},
-    { $sort : {
-        "borough":1
-    }}
-] )
+> db.restaurants.aggregate( [
+>     { $project : {
+>         "name":1, "borough":1, "_id":0
+>     }},
+>     { $sort : {
+>         "borough":1
+>     }}
+> ] )
 
 # Q : un grade "C" par quartier ? 
 # Indice : la dernière inspection : la plus récente, donc la première de La répartition des restaurants dont la dernière inspection a donné la liste ! # key : "grade" 
 
-db.restaurants.aggregate( [
-    { $match : {
-        "grades.0.grade":"C"
-    }},
-    { $project : {
-        "name":1, "borough":1, "_id":0
-    }},
-    { $group: {
-      _id: 0,
-      last: { $last: "$date" }
-     }
-] )
+> db.restaurants.aggregate( [
+>     { $match : {
+>         "grades.0.grade":"C"
+>     }},
+>     { $project : {
+>         "name":1, "borough":1, "_id":0
+>     }},
+>     { $addFields: {         
+>         lastElem: { $last: "$date" }      
+>     }}
+> ] )
 
 # Q : Calculer le score moyen des resto par quartier et trier par score décroissant ? 
-# Indice : utiliser l'opérateur unwid { "_id" : "Queens", "moyenne" : 11.634865110930088 } { "_id" : "Brooklyn", "moyenne" : 11.447723132969035 } { "_id" : "Manhattan", "moyenne" : 11.41823125728344 } { "_id" : "Staten Island", "moyenne" : 11.370957711442786 } { "_id" : "Bronx", "moyenne" : 11.036186099942562 } { "_id" : "Missing", "moyenne" : 9.632911392405063 }
+# Indice : utiliser l opérateur unwid { "_id" : "Queens", "moyenne" : 11.634865110930088 } { "_id" : "Brooklyn", "moyenne" : 11.447723132969035 } { "_id" : "Manhattan", "moyenne" : 11.41823125728344 } { "_id" : "Staten Island", "moyenne" : 11.370957711442786 } { "_id" : "Bronx", "moyenne" : 11.036186099942562 } { "_id" : "Missing", "moyenne" : 9.632911392405063 }
+
+> varUnwind = {$unwind : "$grades"}
+> varGroup4 = { $group : {"_id" : "$borough", "moyenne" : {$avg : "$grades.score"} } };
+> varSort2 = { $sort : { "moyenne" : -1 } }
+> db.restaurants.aggregate( [ varUnwind, varGroup4, varSort2 ] );
 
 ##### Exercice Aggréger des transactions ##### 
-Q : Créer une collection "transactions" à partir du fichier transactions.json et répondre aux questions suivantes : 
+Q : Créer une collection "transactions" à partir du fichier transactions.json et répondre aux questions suivantes :
+
+> db.createCollection("transactions")
+
+> mongoimport -d db_agg -c transactions --drop --file "D:/Cours/B3/NoSQL/TP3/tp3json/transactions.json" --jsonArray
+
+
 Q-1. Calculer le montant total des paiements ? 
 # Indice : key : Payment.Total 
 Q-2. Calculer le montant total par paiment ? 
 Q-3. Calculate total payments (Payment.Total) for each payment type (Payment.Type) ? 
-Q-4. Trouver l'Id le plus élevé. Q-5. Find the max price (Transaction.price) ?
+Q-4. Trouver l Id le plus élevé. Q-5. Find the max price (Transaction.price) ?
 
-##### distinct ##### 
+##### Distinct ##### 
 # Q : Age unique => distinct ? db.people.distinct("age") 
 # S : [ 20, 35, 60 ] Q : Peut-on faire un distinct sur plusieurs key ? db.people.distinct("name", "age") 
-# R : Ce n'est pas possible de faire un distinct sur plusieurs key => Il faut passer par map reduce !
+# R : Ce n est pas possible de faire un distinct sur plusieurs key => Il faut passer par map reduce !
 
+
+#############################################
 var schematodo = db.restaurants.findOne();
 for (var key in schematodo) { print (key) ; }
